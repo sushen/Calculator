6a8bcdfbc9b1360246da756119ad9160de70ce66
sushen <sushenbiswasaga@gmail.com>
2019-07-19 15:57:36 +0600

    Initial commit

---
+++ app/src/main/java/blog/biswas/video/calculator/MainActivity.java
@@ -0,0 +1,23 @@
+package blog.biswas.video.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvNumber = findViewById(R.id.tvNumber);
+    }
+    public void setText(View view){
+        tvNumber.setText(tvNumber.getText().toString() + view.getTag().toString());
+    }
+}
